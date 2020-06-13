31280329cef6966d8d739e65c1b50faa168c5ff3
Ibrahim <ibrahimalhanich@gmail.com>
2020-06-13 20:56:17 -0300

    OAuth Implementation Complete

---
+++ controllers/users.js
@@ -0,0 +1,19 @@
+const User = require('../models/user');
+module.exports = {
+  index
+};
+function index(req, res, next) {
+  console.log(req.query)
+  console.log("INDEX CONTROLLER USER");
+  // Make the query object to use with Student.find based up
+  // the user has submitted the search form or now
+  let modelQuery = req.query.name ? { name: new RegExp(req.query.name, 'i') } : {};
+  // Default to sorting by name
+  let sortKey = req.query.sort || 'name';
+  User.find(modelQuery)
+    .sort(sortKey).exec(function (err, students) {
+      if (err) return next(err);
+      // Passing search values, name & sortKey, for use in the EJS
+      res.render('users/index', { user: req.user, name: req.query.name });
+    });
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
-
+const passport = require('passport');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('', { title: 'Meme Review' });
 });
+router.get('/auth/google', passport.authenticate(
+  'google',
+  { scope: ['profile', 'email'] }
+));
 
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/users',
+    failureRedirect: '/users'
+  }
+));
+
+router.get('/logout', function (req, res) {
+  req.logout();
+  res.redirect('/users');
+});
 module.exports = router;
